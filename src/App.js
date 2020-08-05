c389777e46b3f700c666acb99f73c4e75dc1e4f3
Joseph Surla <josephsurla@gmail.com>
2020-08-05 17:15:08 +0900

    Create layout of pomodoro timer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>Pomodoro Timer</h1>
+
+      <div className="timer">
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </div>
+
+      <div className="buttons">
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
